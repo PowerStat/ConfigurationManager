839a059bc90659b348b595afa65e36e85082057f
Kai Hofmann <powerstat@web.de>
2023-10-31 13:49:03 +0100

    Updated dependencies, fixed test module-.info

--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,19 +1,25 @@
 /*
  * Copyright (C) 2023 Dipl.-Inform. Kai Hofmann. All rights reserved!
  */
-
+ 
 
 /**
  * Configuration module.
  */
-module de.powerstat.configuration
+open module de.powerstat.configuration
  {
   exports de.powerstat.configuration;
-  exports de.powerstat.configuration.io;
 
   requires de.powerstat.validation;
   requires org.apache.logging.log4j;
   requires java.xml;
   requires com.google.gson;
+
+  requires com.github.spotbugs.annotations;
+  requires org.junit.jupiter.api;
+  requires org.junit.jupiter.params;
+  requires org.junit.platform.launcher;
+  requires org.junit.platform.suite.api;
+  // requires nl.jqno.equalsverifier;
 
  }
